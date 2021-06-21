c1721e8f7db1239cb632efbc54fd9aa63c14a28b
hasan-furkan <hsnfrkn32@gmail.com>
2021-06-21 23:07:09 +0300

    first-lesson

---
+++ first-lesson/app.js
@@ -0,0 +1,39 @@
+// var myName = "Matthew";
+
+
+// console.log("——————");
+// console.log(myName);
+
+
+// myName = "John";
+// console.log(myName);
+// var announce = "welcome to our scholl";
+// console.log(announce);
+
+// annuonce = "Welcome to full stack";
+// console.log(announce);
+
+// const PI = 3.1231;
+// let myNum = 5;
+// console.log(PI);
+// myNum = PI * 4;
+// console.log(myNum);
+
+
+// console.log("hello world!");
+// console.info("some infor");
+// console.warn("some warning");
+// console.error("errorr");
+
+function func1(){
+    var myName = "John";
+    console.log("from func1 ...", myName);
+    if(true){
+        let herName= "Daisy";
+        var yourName= "Melih";
+        console.log(yourName,herName );
+    }
+    console.log(yourName);
+    console.log(yourName, herName);
+}
+func1();
