60c1f45d352e078764df4549b184074d96f6846d
hasan-furkan <hsnfrkn32@gmail.com>
2021-06-24 04:48:07 +0300

    second-lesson

---
+++ second-lesson/app.js
@@ -0,0 +1,69 @@
+    // console.log("Hello app\'js")
+    // console.log("Hello app\'js")
+    // // alert("merhaba")
+    // console.log(a);
+    // var a = 3
+    // console.log(a);
+    // // console.log(b); ilk once tanimlamamiz lazim yoksa hata verir 
+    // let b = 5
+    // console.log(b);
+    // // let b = 4 bu sekilde letde degistirme yapilamaz
+    // b = 4
+    // console.log(b);
+    // const c = 7  // bu hic degistirilemez
+    // console.log(c);
+
+
+
+        // --------------------------NUMBER------------------------
+        // hepsi number olarak geciyor float diye bir ayrilma yok 
+        // maksimum olarak 15 basamakli bir sayi alabilir
+        // eger 15 basamaktan daha fazla yazarsak ust sayiya yuvarlar 
+    // var a = 5
+    // console.log(typeof a);
+    // var d = 321313.1231
+    // console.log(typeof d);
+    // var e = -1231
+    // console.log(typeof e);
+
+    // -----------------------------String--------------
+    // var UserName = "Mark"
+    // var b = `Hello ${UserName}`
+    // console.log(b);
+
+    // var c = "I\'m Ready"
+    // console.log(c);
+
+    // var d = "I'm Furkan"
+    // console.log(d);
+
+    // // bu sekilde css kodu uygulayabiliriz
+    // console.log('%c Hello Full Stack! ', 'background: #840A7F ; color: #fff');
+
+    // ---------------Boolean---------------------------------------
+    // var x = 8 > 4
+    // console.log(x); true
+    
+    // var z = 8 == 7 
+    // console.log(z); false
+
+    // var W = 8 == 8 
+    // console.log(W); true
+
+    // var a = 8 == "8"
+    // console.log(a); true
+
+    // var a = 8 + "8" 88
+    // console.log(a);
+
+    // var x = Boolean(0) false
+    // console.log(x);
+
+    // var x = Boolean(1) true
+    // console.log(x);
+
+    // var x = Boolean("0") true
+    // console.log(x);
+    
+    // var x = Boolean("") false
+    // console.log(x);
