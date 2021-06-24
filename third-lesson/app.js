56921209a57447b719cd8a820c114abab3dab644
hasan-furkan <hsnfrkn32@gmail.com>
2021-06-24 22:48:22 +0300

    third-lesson

---
+++ third-lesson/app.js
@@ -0,0 +1,137 @@
+// let a;
+// let aa;
+// let aaa;
+
+
+//  let abc = function add (a,b){
+//     console.log(a+b);
+// }
+// abc(7,5)
+
+
+// function stack (){
+//     stack()
+
+// }
+// stack()
+
+// let a = 5;
+// console.log(a)
+// {
+//     let a = 8;
+//     console.log(a)
+// }
+// console.log(a)
+
+/* var a = 6;
+var b = 2;
+console.log(a+b);
+var c = 6;
+var d = 2;
+console.log(c-d);
+var e = 6;
+var f = 2;
+console.log(e*f);
+var g = 6;
+var h = 2;
+console.log(g/h);
+var g = 6;
+var h = 3;
+console.log(g**h);
+
+var a = 123;
+var b = 5;
+a = --b  /* -- isareti b'den once geliyorsa 1 cikartip oyle yaziyor */
+// console.log({a});
+// console.log({b});
+
+
+// var a = 123;
+// var b = 2;
+// a = b--  /* -- isareti b'den sonra geliyorsa a'yi b degerini atiyor ve b'den 1 cikartiyor */
+// console.log({a});
+// console.log({b});
+
+
+// var b = 5;
+
+// console.log(b++);
+// console.log(b);
+
+// var b = 5;
+// var a = ++b
+// console.log(b);
+// console.log(a);
+
+// var b = 7;
+// var a = ++b /* ++ isareti b'den once geliyorsa 1 arttirp oyle yaziyor */
+// console.log({b});
+// console.log({a});
+
+// var b = 7;
+// var a = b++     /* ++ isareti b'den sonra geliyorsa a'yi b degerini atiyor ve b'den 1 arttiriyor */
+// console.log({b});
+// console.log({a}); */
+
+// ----------------------Assigment Operators---------------------------
+
+// var a = 6;
+// var b = 2;
+
+// console.log(a = b , {a}, {b});
+// console.log(a += b , {a}, {b});
+// console.log(a -= b , {a}, {b});
+// console.log(a *= b , {a}, {b});
+// console.log(a /= b , {a}, {b});
+// console.log(a %= b , {a}, {b});
+
+
+var a = 2;
+var b = "2";
+console.log(a==b); /*  burada 2 esittir ile degerleri ayni yani true diyor */
+
+var a = 2;
+var b = "2";
+console.log(a===b); /* burada ise 3 esittirle biri string digeri number diyor o yuzden false */
+// console.log('strict equality',a === b) //false
+// console.log('equality',a == b) //true
+// var a = 2;
+// var b = '2';
+// console.log('strict inequality',a !== b) // true
+// console.log('inequality',a != b) //false
+// let age = 14;
+// console.log(age >= 14) //true
+// console.log(age > 14) //false
+// console.log(false == false) //true
+// console.log(Boolean(0) == false) //true
+// console.log(Boolean('0') == false) //false
+// console.log(0 == false) // true
+// console.log('0' == false) //true
+// console.log(typeof null) //object
+// console.log(null == false) //false
+// console.log(null == true) //false
+// console.log(null+3) //3
+// console.log(null == 0) //false
+// console.log(Boolean(null) == false) //true
+// console.log(Boolean(null) == Boolean(0)) //true
+// console.log(typeof NaN)
+//isNaN
+// Kullanıcıdan alınan veri sayı mmı değil mi?
+// var a ;
+// while(isNaN(a)){
+//     a = prompt("Enter a Number")
+//     a = Number(a)
+//     console.log(a, typeof a)
+// }
+// console.log(015+025) //34 = 13 + 21
+// console.log(15+025) //36 = 15 + 21
+// console.log(015+25) //38 = 13 + 25
+// console.log(15+25) //40
+// console.log(0015+0025)
+// 0 1 2 3 4 5 6 7
+// console.log(9 < 8) //false
+// console.log( 9 < 8 < 7 ) // true
+// console.log(false < 7) //true
+// console.log('Clarusway'> 'clarusway') //false
+console.log('A'.charCodeAt())
+console.log('a'.charCodeAt())
