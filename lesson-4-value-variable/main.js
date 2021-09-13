af62937482045664955f9500d991cfb1e38ce578
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2021-09-13 12:26:07 +0300

    more-lessons

---
+++ lesson-4-value-variable/main.js
@@ -0,0 +1,16 @@
+// var x = 5;
+// var y = 10;
+// var c = x * y;
+// var isim ="emre";
+// var sayi = "5"
+// console.log(c);
+// document.writeln(x + "<br/>")
+// document.writeln(y + "<br/>")
+// document.writeln(c + "<br/>")
+// document.writeln(isim + "<br/>")
+// document.writeln("kullanici ismi" + isim + "<br/>")
+// document.writeln(sayi + 10 + "<br/>") 
+
+// Detayli Degisken Kullanimi !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+document.write("Merhaba <br/>")
+var isim = "lorem30"
